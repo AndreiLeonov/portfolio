56ca86ce9a8abb5c82901e932226b950bb910a2d
AndreiLeonov <andrei_leonov93@mail.ru>
2020-10-05 22:11:55 +0300

    Updates

---
+++ precache-manifest.f8b01e20d1dc2986c21efde27a90f5e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5db3a5683594a3e013e7d54573ca4d07",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "220aef29e0a6ba6884e5",
+    "url": "/portfolio/static/css/main.d1b61cbe.chunk.css"
+  },
+  {
+    "revision": "08cba435d7b5557bd678",
+    "url": "/portfolio/static/js/2.feec956e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.feec956e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "220aef29e0a6ba6884e5",
+    "url": "/portfolio/static/js/main.43b2b8c5.chunk.js"
+  },
+  {
+    "revision": "361b25be0752ed4bc489",
+    "url": "/portfolio/static/js/runtime-main.b679d28b.js"
+  },
+  {
+    "revision": "677d3abf75ddc6139ac411467c792eef",
+    "url": "/portfolio/static/media/photo.677d3abf.png"
+  }
+]);
